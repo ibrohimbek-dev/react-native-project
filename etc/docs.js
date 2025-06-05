7dd339022131bc1330b502e03a1e400efb6dd45c
Ibrohimbek <programming0127@gmail.com>
2025-06-06 06:19:55 +0900

    feat: 2025-06-06 - 06: 19

---
+++ etc/docs.js
@@ -0,0 +1,10 @@
+// 2025-06-04:
+
+// Installing React Native App:
+// npx create-expo-app --template blank react-native-project
+
+
+
+// Reset:
+// npm start -- --reset-cache
+// npm install ; npm start
